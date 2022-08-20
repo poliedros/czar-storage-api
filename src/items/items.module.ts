ff58e03245f199e3c94e5a4791d62643f8d3ea52
Carlos <carloshlzansavio@gmail.com>
2022-08-20 02:36:42 +0100

    feat: update project

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -5,16 +5,19 @@ import type { ClientOpts as RedisClientOpts } from 'redis';
 import * as redisStore from 'cache-manager-redis-store';
 import { ItemsController } from './items.controller';
 
+const REDIS = 1;
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     ClientsModule.register([
       {
         name: 'ITEMS_SERVICE',
-        transport: Transport.REDIS,
+        transport: REDIS,
         options: {
-          url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-          auth_pass: `${process.env.REDIS_AUTH_PASS}`,
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
+          password: `${process.env.REDIS_AUTH_PASS}`,
         },
       },
     ]),
--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -3,16 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { StorageController } from './storage.controller';
 
+const REDIS = 1;
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     ClientsModule.register([
       {
         name: 'STORAGE_SERVICE',
-        transport: Transport.REDIS,
+        transport: REDIS,
         options: {
-          url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-          auth_pass: `${process.env.REDIS_AUTH_PASS}`,
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
+          password: `${process.env.REDIS_AUTH_PASS}`,
         },
       },
     ]),
