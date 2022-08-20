987e63189dbff144648cf031c66b7dd37b1ddb6f
Carlos <carloshlzansavio@gmail.com>
2022-08-20 02:19:40 +0100

    fix: remove catalog

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
-import { CatalogModule } from './catalog/catalog.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HealthModule } from './health/health.module';
@@ -8,20 +6,7 @@ import { ItemsModule } from './items/items.module';
 import { StorageModule } from './storage/storage.module';
 
 @Module({
-  imports: [
-    CatalogModule,
-    RouterModule.register([
-      {
-        path: 'catalog',
-        module: CatalogModule,
-      },
-    ]),
-    AuthModule,
-    UsersModule,
-    HealthModule,
-    ItemsModule,
-    StorageModule,
-  ],
+  imports: [AuthModule, UsersModule, HealthModule, ItemsModule, StorageModule],
   controllers: [],
   providers: [],
 })
